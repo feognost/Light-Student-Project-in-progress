a27b660aaf1dc7bc4dc2a62a1bdf4e660cfc4eb1
Vladislav Daragan <Vladislav Daragan>
2020-06-05 20:55:38 +0200

    Guide Project - Light

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -5,31 +5,46 @@
 //  Created by Vladislav Daragan on 05.06.20.
 //  Copyright © 2020 Vladislav Daragan. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
     
-    var lightOn = false
+    var lightOn = true
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         lightOn = !lightOn
         
-        updateUI()
+        updateView()
     }
-    func updateUI() {
+    
+    func updateView() {
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if let dev = device, dev.hasTorch {
+            view.backgroundColor = .black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = lightOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
         view.backgroundColor = lightOn ? .white : .black
-                       
+        }
                   }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        updateUI()
-    }
+   
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           updateView()
+       }
 }
 
 
